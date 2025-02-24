7a3875500ad86842fb4ab9c68692a8d3b2df442d
MahsaBazzaz <mahsa.bazzaz@gmail.com>
2025-02-24 16:11:11 -0500

    background music as autoload

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
 ## Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
@@ -12,4 +13,5 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
 	queue_free()
